26e1f1404a54d56da9296b209fd89cc201e04612
teydd <chemosted@gmail.com>
2026-04-01 23:56:40 +0300

    landing page button update to link

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <div className="mt-5 place-items-center justify-center flex md:flex-row gap-4 ">
           <div className="grid md:grid-cols-2 grid-cols-1 grow bg-lime-200/60 px-3 md:w-2/5 md:px-20 rounded-2xl container">
             <div className="p-6 flex items-center justify-center">
-              <Image className="rounded-2xl md:w-auto" src={"/connector.png"} width={500} height={500} alt="Database connector illustration picture"/>
+              <Image className="rounded-2xl md:w-auto backdrop-blur-3xl" src={"/connector.png"} width={500} height={500} alt="Database connector illustration picture"/>
             </div>
             <div className="px-10 flex justify-center flex-col">
               <p className="lg:text-2xl md:text-4xl text-2xl">
@@ -21,7 +21,7 @@ export default function Home() {
               </p>
               <p className="text-xl lg:text-xl md:text-3xl">Your connections simplified, Your insights amplified</p>
               <p className="font-light lg:text-lg md:text-4xl">Plug in, power up and let your database talk seamlessly</p>
-              <Button className="mt-10">Log In</Button>
+              <Button className="mt-10"><Link href={"/signin"}>Sign In</Link></Button>
             </div>
           </div>
         </div>
