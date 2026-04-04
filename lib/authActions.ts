0dec32cf3b97e0a3d95c0f99f09ad95541aa3a1f
teydd <chemosted@gmail.com>
2026-04-04 21:56:22 +0300

    signup action

---
+++ lib/authActions.ts
@@ -0,0 +1,31 @@
+"use server";
+
+import { AuthResponse, LoginData, RegisterData } from "@/types/auth";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+const BASE = process.env.BASE_URL;
+
+export const registerUser =  async (formData: FormData) => {
+  const data = {
+    email: formData.get("email"),
+    username: formData.get("username"),
+    password: formData.get("password"),
+  };
+  const res = await fetch(`${BASE}/register/`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  if (!res.ok) {
+    throw new Error("Registration Failed");
+  }
+  const result = await res.json();
+  redirect("/signin");
+
+  return result;
+};
+
+
