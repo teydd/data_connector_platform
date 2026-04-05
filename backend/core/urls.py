3034ee8f5df66f1d980460b8edfe978b67b5b59f
teydd <chemosted@gmail.com>
2026-04-05 05:08:02 +0300

    added the admin route

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,6 +1,8 @@
+from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path("api/", include('accounts.urls')),
     path('api/login/', TokenObtainPairView.as_view()),]
