a9632546e698f6d80e35e37959f11c42bdcaa26f
teydd <chemosted@gmail.com>
2026-04-05 09:38:57 +0300

    regitered the user model i created on how to display user data

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'role', 'is_active', 'date_joined']
+    list_filter = ['role', 'is_active', 'date_joined']
+    search_fields = ['username', 'email']
+    ordering = ['-date_joined']
+    readonly_fields = ['date_joined', 'last_login']
+    
+    fieldsets = (
+        ('Personal Info', {
+            'fields': ('username', 'email', 'first_name', 'last_name')
+        }),
+        ('Permissions', {
+            'fields': ('role', 'is_active', 'is_staff', 'is_superuser')
+        }),
+        ('Important dates', {
+            'fields': ('date_joined', 'last_login'),
+            'classes': ('collapse',)
+        }),
+    )
