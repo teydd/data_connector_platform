b38b1887085ff609651f42d42f2231098bfb42b8
teydd <chemosted@gmail.com>
2026-04-06 03:29:30 +0300

    connections app with updated model

---
+++ backend/connections/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import Connection
+
+admin.site.register(Connection)
---
+++ backend/connections/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.conf import settings
+class Connection(models.Model):
+    DB = [
+        ('postgres', 'PostgreSQL'),
+        ('mysql', 'MySQL'),
+        ('mongodb', 'MongoDB'),
+        ('clickhouse', 'ClickHouse')
+    ]
+
+    name = models.CharField(max_length=100)
+    database_type = models.CharField(max_length=20, choices=DB)
+
+    host = models.CharField(max_length=255, blank=True, null=True)
+    port = models.IntegerField(blank=True, null=True)
+
+    username = models.CharField(max_length=255, blank=True, null=True)
+    password = models.TextField(blank=True, null=True)
+
+    database_name = models.CharField(max_length=255, blank=True, null=True)
+
+    uri = models.TextField(blank=True , null=True)
+
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
