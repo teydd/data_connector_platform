d0587fa421f41bec7ec5ffdcf8ff6a29d40ae5e3
teydd <chemosted@gmail.com>
2026-04-07 21:48:06 +0300

    database connection  serializers

---
+++ backend/connections/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Connection
+
+class DatabaseSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Connection
+        fields = '__all__'
+        read_only_fields = ['id', 'created_at', 'updated_at']
