804284dca4eb5c09f1aa01830ed7d0775422074b
teydd <chemosted@gmail.com>
2026-04-07 21:53:20 +0300

    added sql as the default database

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -74,9 +74,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME' : 'connector_db',
+        'PORT' : '5432',
+        'HOST' : 'localhost',
+        'USERNAME' : 'kiddyt',
+        'PASSWORD' : "teyddie2001"
+    },
+    
 }
 
 
