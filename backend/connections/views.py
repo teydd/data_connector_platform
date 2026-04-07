048adeb08bd42a2c27d3815ebb7692be7d9ab845
teydd <chemosted@gmail.com>
2026-04-07 21:55:05 +0300

    connections view

--- backend/connections/views.py
+++ backend/connections/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics,status
+from rest_framework.response import Response
+from .models import Connection
+from .serializers import DatabaseSerializers
+from .drivers.factory import get_connector
 
-# Create your views here.
+class ConnectionListCreateView(generics.ListCreateAPIView):
+    #Creates and Posts requests
+    queryset = Connection.objects.all()
+    serializer_class = DatabaseSerializers
+
+class ConnectionRetrieveUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
+    #Get Update Deleting requests
+    queryset = Connection.objects.all()
+    serializer_class = DatabaseSerializers
