5e2b9bc3cb7e8f77221b916a9fde60b02f2135de
teydd <chemosted@gmail.com>
2026-04-07 21:51:13 +0300

    database connector

--- backend/connections/drivers/factory.py
+++ backend/connections/drivers/factory.py
@@ -1,20 +1,16 @@
-# db_drivers/factory.py
+from .mongodb import MongoDBConnector
+from .clickhouse import ClickHouseConnector
+from .mysql import MySqlConnector
+from .postgres import PostgresqlConnector
 
-from .postgres import PostgresDriver
-from .mongodb import MongoDriver
-
-class DriverFactory:
-
-    DRIVERS = {
-        "postgres": PostgresDriver,
-        "mongodb": MongoDriver,
-    }
-
-    @classmethod
-    def get_driver(cls, db_connection):
-        driver_class = cls.DRIVERS.get(db_connection.db_type)
-
-        if not driver_class:
-            raise Exception("Unsupported DB type")
-
-        return driver_class(db_connection)
+def get_connector(config):
+    if config.database_type == 'postgresql':
+        return PostgresqlConnector(config)
+    elif config.database_type == 'mysql':
+        return MySqlConnector(config)
+    elif config.database_type == 'clickhouse':
+        return ClickHouseConnector(config)
+    elif config.database_type == 'mongodb':
+        return MongoDBConnector(config) 
+    else:
+        return ValueError("Unsupported database type")
