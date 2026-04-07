d22a2139c185e23f7f8b0779c839054e44849211
teydd <chemosted@gmail.com>
2026-04-07 21:54:27 +0300

    added the connections url route to the core folder

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include('accounts.urls')),
     path('api/login/', TokenObtainPairView.as_view()),
+    path('api/connections/', include('connections.urls'))
     ]
