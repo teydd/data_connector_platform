a14d20bf26b5d38348355069e6b33b32d235f1e8
teydd <chemosted@gmail.com>
2026-04-07 11:42:18 +0300

    abstract methods

--- backend/connections/drivers/root.py
+++ backend/connections/drivers/root.py
@@ -2,12 +2,19 @@ from abc import ABC, abstractmethod
 
 class RootDriver(ABC):
 
+    def __init__(self, config):
+        self.config = config
+
     @abstractmethod
     def connect(self):
         pass
 
     @abstractmethod
     def test_connection(self):
+        pass
+
+    @abstractmethod
+    def query(self, query):
         pass
 
     @abstractmethod
