2812db65fbc133fead6e69f7423b36120e6b7ed0
teydd <chemosted@gmail.com>
2026-04-09 01:10:33 +0300

    update

--- backend/connections/drivers/root.py
+++ backend/connections/drivers/root.py
@@ -4,6 +4,7 @@ class RootDriver(ABC):
 
     def __init__(self, config):
         self.config = config
+        self.connection = None
 
     @abstractmethod
     def connect(self):
@@ -15,6 +16,14 @@ class RootDriver(ABC):
 
     @abstractmethod
     def query(self, query):
+        pass
+
+    @abstractmethod
+    def fetch_tables(self):
+        pass
+
+    @abstractmethod
+    def fetch_data(self, table: str, batch_size: int = 100, offset: int=0):
         pass
 
     @abstractmethod
