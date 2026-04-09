ba335b9475d1ddbdb751958ed128be452d5e80d8
teydd <chemosted@gmail.com>
2026-04-09 10:39:06 +0300

    update on the postgresql connector

--- backend/connections/drivers/postgres.py
+++ backend/connections/drivers/postgres.py
@@ -11,27 +11,58 @@ class PostgresqlConnector(RootDriver):
             port = self.config['port'],
             user = self.config['username'],
             password = self.config['password'],
-            database = self.config['database_name']
+            database = self.config['database_name'],
+            connect_timeout=10
         )
-
-        self.cursor = self.conn.cursor()
         print("Connected to PostgreSQL")
 
     def query(self,query):
-        self.cursor.execute(query)
-        return self.cursor.fetchall()
+        cursor = self.conn.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
     
 
     def test_connection(self):
         try:
-            self.cursor.execute("SELECT 1")
+            cursor = self.conn.cursor()
+            cursor.execute("SELECT 1")
             print("Connected to PostgreSQL successfully")
             return True
         except Exception as e:
-            print(f'Unsuccessful connection to PostgreSQL')
+            print('Unsuccessful connection to PostgreSQL')
             return False
+        
+    def fetch_tables(self):
+       cursor = self.conn.cursor()
+       cursor.execute("""
+                SELECT table_name
+                FROM information_schema.tables
+                WHERE table_schema='public'
+            """)
+       return [row[0] for row in cursor.fetchall()]
+    
+    def fetch_data(self, table: str, batch_size: int = 100, offset: int = 0) -> dict:
+        cursor = self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+
+        cursor.execute(f'SELECT COUNT(*) FROM "{table}"')
+        total = cursor.fetchone()['count']
+
+        cursor.execute(
+            f'SELECT * FROM "{table}" LIMIT %s OFFSET %s',
+            (batch_size, offset)
+        )
+        rows = cursor.fetchall()
+
+        columns = [desc[0] for desc in cursor.description]
+
+        return {
+            "columns": columns,
+            "rows": rows,
+            "total": total
+        }
+        
+
 
     def close(self):
-        self.cursor.close()
         self.conn.close()
         print("PostgreSQL connection closed")
