7c2d9ff372ddb7a6ffb0f489a79cec92d9e83731
teydd <chemosted@gmail.com>
2026-04-10 14:16:43 +0300

    added the updated by field

--- backend/connections/models.py
+++ backend/connections/models.py
@@ -22,7 +22,9 @@ class Connection(models.Model):
     uri = models.TextField(blank=True , null=True)
 
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.name
+        return f"{self.name} ({self.database_type})"
