026ca109171b3dfd0476fc528fdc3ddcd49a7e02
teydd <chemosted@gmail.com>
2026-04-10 19:19:37 +0300

    updated the views according to the serializers

--- backend/connections/views.py
+++ backend/connections/views.py
@@ -1,13 +1,13 @@
 from rest_framework import generics,status
 from rest_framework.response import Response
 from .models import Connection
-from .serializers import DatabaseSerializers
+from .serializers import DatabaseSerializers, DatabaseCreateSerializer
 from .drivers.factory import get_connector
 
 class ConnectionListCreateView(generics.ListCreateAPIView):
     #Creates and Posts requests
     queryset = Connection.objects.all()
-    serializer_class = DatabaseSerializers
+    serializer_class = DatabaseCreateSerializer
 
 class ConnectionRetrieveUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
     #Get Update Deleting requests
