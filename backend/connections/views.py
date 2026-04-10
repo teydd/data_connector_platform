db48bf0ce69b743655ea6142cb24371cb681d43a
teydd <chemosted@gmail.com>
2026-04-10 19:24:35 +0300

    update

--- backend/connections/views.py
+++ backend/connections/views.py
@@ -1,8 +1,6 @@
-from rest_framework import generics,status
-from rest_framework.response import Response
+from rest_framework import generics
 from .models import Connection
 from .serializers import DatabaseSerializers, DatabaseCreateSerializer
-from .drivers.factory import get_connector
 
 class ConnectionListCreateView(generics.ListCreateAPIView):
     #Creates and Posts requests
