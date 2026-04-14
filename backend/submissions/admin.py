d3bbaf321a402bc07c5bc0794f7190ff8e2bcdfe
teydd <chemosted@gmail.com>
2026-04-14 20:33:49 +0300

    update

--- backend/submissions/admin.py
+++ backend/submissions/admin.py
@@ -3,5 +3,5 @@ from .models import Submission
 
 @admin.register(Submission)
 class SubmissionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'database_type', 'repo_link', 'submitted_at')
-    search_fields = ('user__username', 'repo_link')
+    list_display = ('id', 'user','submitted_at')
+    search_fields = ('user__username',)
--- backend/submissions/models.py
+++ backend/submissions/models.py
@@ -4,27 +4,16 @@ from connections.models import Connection
 
 
 class Submission(models.Model):
-    DB = [
-        ('postgresql', 'PostgreSQL'),
-        ('mysql', 'MySQL'),
-        ('mongodb', 'MongoDB'),
-        ('clickhouse', 'ClickHouse'),
-    ]
     user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
-    database_type = models.CharField(
-    max_length=20,
-    choices=DB,
-    null=True,
-    blank=True,
-    default=None
-)
+    connection = models.ForeignKey(Connection, null=True, blank=True, on_delete=models.CASCADE)
+    table = models.CharField(max_length=255, null=True, blank=True)
+    collection = models.CharField(max_length=255, null=True, blank=True)
     title = models.CharField(max_length=255, null=True, blank=True)
     file_upload = models.FileField(upload_to="submissions/", blank=True)
-    repo_link = models.URLField(blank=True, null=True)
-    video_link = models.URLField(blank=True, null=True)
+    metadata = models.JSONField(null=True, blank=True)
     documentation = models.TextField(blank=True)
     submitted_at = models.DateTimeField(auto_now_add=True, null=True)
 
 
     def __str__(self):
-        return f"{self.user} -> {self.database_type}"
+        return f"{self.user} -> {self.connection}"
